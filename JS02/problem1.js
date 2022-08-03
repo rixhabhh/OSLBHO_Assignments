f5a1aa970e48b15fef43517b5a1caef73e63176c
rixhabhh <rishabh.vishwakarma@opensenselabs.com>
2022-08-03 19:04:12 +0530

    JS ASSIGNMENT #2

---
+++ JS02/problem1.js
@@ -0,0 +1,10 @@
+
+function tellFortune(x,y,z,n){
+    return "You will be a "+ x +" in " + y + ", and married to " + z + " with " + n + " kids.";
+}
+
+
+console.log(tellFortune("Engineer","Delhi","Priya",1))
+console.log(tellFortune("Doctor","California","Angela Yu",2))
+console.log(tellFortune("Astranaut","Beverly Hills","Bernadette",3))
+
---
+++ JS02/problem2.js
@@ -0,0 +1,13 @@
+function calcCircumfrence(r){
+    
+    return "The circumference is "+ 2*r*(22/7);
+}
+
+function calcArea(r){
+    
+    return "The area is "+ (22/7)*r*r;
+}
+
+console.log(calcCircumfrence(10));
+
+console.log(calcArea(10));
